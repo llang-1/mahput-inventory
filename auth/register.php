4caf7fb68ac23de3a5ea22a5da94465897ff488d
llng <langzzstorelaxy@gmail.com>
2026-05-06 21:39:42 +0700

    register html form

---
+++ auth/register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register | Mahput Inventory</title>
+</head>
+
+<body>
+    <pre>
+        <form action="" method="post">
+            <input type="text" name="nis" placeholder="Nis">
+            <input type="text" name="nama" placeholder="Nama">
+            <input type="text" name="password" placeholder="Password">
+            <select name="jurusan" aria-placeholder="jurusan">
+                <option value="#" disabled selected="hidden">Pilih jurusan</option>
+                <option value="PPLG">PPLG</option>
+                <option value="DKV">DKV</option>
+            </select>
+            <input type="text" name="no_tlp" placeholder="No_tlp">
+            <textarea type="text" name="alamat "placeholder="Alamat"></textarea>
+            <input type="text" name="kelas" placeholder="Kelas">
+        </form>
+        
+    </pre>
+</body>
+
+</html>
